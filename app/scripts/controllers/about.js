814574ba834e046e4ec81587c4c34a28f38aced2
MarinAngelo <marinus.klap@gmail.com>
2014-09-30 21:24:25 +0200

    leer

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mwdBeispielProjektApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mwdBeispielProjektApp
+ */
+angular.module('mwdBeispielProjektApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
