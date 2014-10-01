86955290d4d61090b12a56479a34793dc9761d9f
MarinAngelo <marinus.klap@gmail.com>
2014-10-01 15:13:12 +0200

    neu ohne sass

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration
 // http://karma-runner.github.io/0.12/config/configuration-file.html
-// Generated on 2014-09-30 using
+// Generated on 2014-10-01 using
 // generator-karma 0.8.3
 
 module.exports = function(config) {
@@ -25,7 +25,6 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
