6ddd09e274b6f34ea469ce27aac0e894e223acc8
MarinAngelo <marinus.klap@gmail.com>
2015-01-20 09:04:02 +0100

    neue index.html

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,19 @@
+'use strict';
+
+angular.module('adressApp', ['ngRoute']);
+  app.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html'
+      })
+      .when('/profilDetails', {
+        templateUrl: 'views/profilDetails.html'
+      })
+      .when('/profilNeu', {
+        templateUrl: 'views/profilNeu.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
 
