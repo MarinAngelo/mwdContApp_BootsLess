e39715d97b21ff079544112ce28f3c6c4dfb84bc
Marinus Klap <marinus@klap.ch>
2015-03-13 15:24:22 +0100

    id als Identifyer eingeführt

--- app/js/controllers/adressDetail.js
+++ app/js/controllers/adressDetail.js
@@ -5,11 +5,11 @@ angular.module('adressApp')
 .controller('AdressDetailCtrl', ['$scope', '$routeParams', '$http',
  function($scope, $routeParams, $http) {
     //Name wird aus den Routeparameter der URL ausgelesen und übergeben
-    $scope.name = $routeParams.AdressName;
+    $scope.id = $routeParams.AdressName;
     //Daten werden nach dem entsprechenden Namen gefiltert und angezeigt
     $http.get('adressen.json').success(function(data) {
         $scope.adresse = data.filter(function(entry) {
-            return entry.name === $scope.name;
+            return entry.id === $scope.id;
         }) [0];
     });
 }]);
