94f70f0a674ccd7cae148595878ce40fe3118270
Marinus Klap <marinus@klap.ch>
2015-03-27 11:12:01 +0100

    stand der dinge

--- app/js/controllers/detail.js
+++ app/js/controllers/detail.js
@@ -1,56 +1,59 @@
 angular.module('adressApp')
-.controller('DetailController', ['$scope', '$routeParams',
- function($scope, $routeParams) {
+    .controller('DetailController', ['$scope', '$routeParams',
+        function($scope, $routeParams) {
 
-    $scope.setDetail = function($routeParams) {
-      $scope.currentDetail = contact;
-    };
+            // $scope.setDetail = function(contact) {
+            //   $scope.currentDetail = contact;
+            // };
+            $scope.currentDetail = null;
+            $scope.currentDetail = $routeParams.AdressName;
 
-    $scope.clearDetail = function() {
-      $scope.currentDetail = null;
-    };
+            // $scope.clearDetail = function() {
+            //     $scope.currentDetail = null;
+            // };
 
- 	  $scope.removeContact = function(contact) {
-      WP.del(contact);
-      reload();
-    };
+            $scope.removeContact = function(contact) {
+                WP.del(contact);
+                reload();
+            };
 
-    $scope.editing = false;
+            $scope.editing = false;
 
-    $scope.toggleEditing = function() {
-      if($scope.editing) {
-        save();
-      }
-      $scope.editing = !$scope.editing;
-    };
+            $scope.toggleEditing = function() {
+                if ($scope.editing) {
+                    save();
+                }
+                $scope.editing = !$scope.editing;
+            };
 
-    function reload() {
-      if($scope.currentDetail) {
-        WP.retrieve.addresses($scope.currentDetail, function(addresses) {
-          $scope.addresses = addresses;
-        });
-        WP.retrieve.phones($scope.currentDetail, function(phones) {
-          $scope.phones = phones;
-        });
-      } else {
-        $scope.addresses = [];
-        $scope.phones = [];
-        $scope.editing = false;
-      }
-      $scope.newAddresses = [];
-    }
-    reload();
+            function reload() {
+                if ($scope.currentDetail) {
+                    WP.retrieve.addresses($scope.currentDetail, function(addresses) {
+                        $scope.addresses = addresses;
+                    });
+                    WP.retrieve.phones($scope.currentDetail, function(phones) {
+                        $scope.phones = phones;
+                    });
+                } else {
+                    $scope.addresses = [];
+                    $scope.phones = [];
+                    $scope.editing = false;
+                }
+                $scope.newAddresses = [];
+            }
+            reload();
 
-    function save() {
-      $scope.addresses.forEach(WP.save.bind(WP));
-      $scope.phones.forEach(WP.save.bind(WP));
-      $scope.newAddresses.forEach(WP.add.address.bind(null, $scope.currentDetail));
-      WP.save($scope.currentDetail);
-    }
-    $scope.$watch('currentDetail', reload);
+            function save() {
+                $scope.addresses.forEach(WP.save.bind(WP));
+                $scope.phones.forEach(WP.save.bind(WP));
+                $scope.newAddresses.forEach(WP.add.address.bind(null, $scope.currentDetail));
+                WP.save($scope.currentDetail);
+            }
+            $scope.$watch('currentDetail', reload);
 
-    $scope.addAddress = function() {
-      $scope.newAddresses.push(WP.create.address());
-    };
+            $scope.addAddress = function() {
+                $scope.newAddresses.push(WP.create.address());
+            };
 
-}]);
+        }
+    ]);
--- app/js/controllers/list.js
+++ app/js/controllers/list.js
@@ -20,4 +20,13 @@ $scope.currentDetail = null;
     }
     reload();
 
+        $scope.newContact = WP.create.entry();
+    $scope.editing = null;
+
+    $scope.addContact = function() {
+      WP.save($scope.newContact);
+      reload();
+      $scope.newContact = WP.create.entry();
+    };
+
 }]);
